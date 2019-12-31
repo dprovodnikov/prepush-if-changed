fce59942063ff9a7a855d2571d929b750aa749ac
Denys Provodnikov <dp.wireden@gmail.com>
2019-12-31 17:19:19 +0200

    Rewrite gitChangedFiles mock

--- src/executeIfMatches.spec.js
+++ src/executeIfMatches.spec.js
@@ -1,21 +1,26 @@
 import 'babel-polyfill';
 
 jest.mock('exec-sh', () => jest.fn());
-jest.mock('git-changed-files', () => () => {
-  return Promise.resolve({
-    committedFiles: ['package.json', 'src/index.js'],
-  });
-});
+jest.mock('git-changed-files', () => jest.fn());
 
 import execSpy from 'exec-sh';
+import gitChangedFilesSpy from 'git-changed-files';
+
 import { executeIfMatches } from './executeIfMatches';
 
 describe('executeIfMatches()', () => {
   beforeEach(() => {
     execSpy.mockClear();
+    gitChangedFilesSpy.mockClear();
   });
 
   test('should execute command if any files match the pattern', async () => {
+    gitChangedFilesSpy.mockReturnValue(
+      Promise.resolve({
+        committedFiles: ['package.json', 'src/index.js'],
+      })
+    );
+
     await executeIfMatches({
       '**/*.js': 'npm run test',
     });
@@ -24,11 +29,30 @@ describe('executeIfMatches()', () => {
   });
 
   test('should not execute command if no files match the pattern', async () => {
+    gitChangedFilesSpy.mockReturnValue(
+      Promise.resolve({
+        committedFiles: ['package.json', 'src/index.css'],
+      })
+    );
+
     await executeIfMatches({
-      '**/*.css': 'echo Hello, World!',
+      '**/*.js': 'echo Hello, World!',
     });
 
     expect(execSpy).not.toBeCalled();
+  });
+
+  test('should not execute command if gitChangedFiles rejects', async () => {
+    gitChangedFilesSpy.mockReturnValue(
+      Promise.reject(Error('Unexpected error'))
+    );
+
+    try {
+      await executeIfMatches(undefined);
+      expect(false).toBeTruthy();
+    } catch (err) {
+      expect(true).toBeTruthy();
+    }
   });
 
   test('should reject if no config was passed on input', async () => {
