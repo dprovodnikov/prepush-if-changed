b1976e026f03f9a81b2dfb692ab2704c20b8c8c7
Denys Provodnikov <dp.wireden@gmail.com>
2019-12-31 16:52:32 +0200

    Use cosmicconfig to load config from various places including package.json

--- cli.js
+++ cli.js
@@ -1,7 +1,27 @@
 #!/usr/bin/env node
 
+const { cosmiconfig } = require('cosmiconfig');
 const executeIfMatches = require('./dist/executeIfMatches').default;
 
-executeIfMatches({
-  'src/**/*': 'npm run test',
-});
+const loadConfig = () => {
+  return cosmiconfig('prepush-if-changed', {
+    searchPlaces: [
+      'package.json',
+      '.prepushrc',
+      '.prepushrc.json',
+      '.prepushrc.yaml',
+      '.prepushrc.yml',
+      '.prepushrc.js',
+      'prepush.config.js',
+    ]
+  })
+  .search()
+  .then(result => result.config);
+};
+
+loadConfig()
+.then(config => {
+  console.log(config);
+  return config;
+})
+.then(executeIfMatches);
