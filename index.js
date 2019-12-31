926dac73d3a4dc12dc5324de445a0f1444bc1686
Denys Provodnikov <dp.wireden@gmail.com>
2019-12-31 11:12:39 +0200

    Move to async/await

--- index.js
+++ index.js
@@ -3,29 +3,25 @@ const getCurrentBranchName = require('git-branch');
 const micromatch = require('micromatch');
 const { run } = require('runjs');
 
-/* eslint-disable */
+const executeBeforePush = async (config) => {
+  const currentBranch = await getCurrentBranchName();
+  const diff = await gitChangedFiles({ baseBranch: currentBranch });
+  const filenames = diff.committedFiles;
 
-const getCommitedFilenames = () => {
-  return getCurrentBranchName()
-    .then(branch => gitChangedFiles({ baseBranch: `origin/${branch}` }))
-    .then(diff => diff.committedFiles);
-};
+  try {
+    Object.keys(config).map((pattern) => {
+      const match = micromatch(filenames, [pattern]);
 
-const executeBeforePush = (config) => {
-  getCommitedFilenames()
-    .then((filenames) => {
-      Object.keys(config).map((pattern) => {
-        const match = micromatch(filenames, [pattern]);
-
-        if (match.length) {
-          run(config[pattern]);
-        }
-      });
-    })
-    .catch(() => process.exit(1));
+      if (match.length) {
+        run(config[pattern]);
+      }
+    });
+  } catch (err) {
+    process.exit(1);
+  }
 };
 
 // TODO: move the config definition to package.json or prepushrc.js
 executeBeforePush({
-  '*.js': '$echo "Hello world"',
+  '*': '$echo "Hello world"',
 });
