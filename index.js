696c8ad0a4b986cef2587778a41d15fbc6a1ead4
Denys Provodnikov <dp.wireden@gmail.com>
2019-12-31 11:30:27 +0200

    Add error handling for a non-existing remote branch to get a diff against.

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const gitChangedFiles = require('git-changed-files');
 const getCurrentBranchName = require('git-branch');
 const micromatch = require('micromatch');
+
+// TODO: replace with a custom implementation
 const { run } = require('runjs');
 
-const executeBeforePush = async (config) => {
-  const currentBranch = await getCurrentBranchName();
-  const diff = await gitChangedFiles({ baseBranch: currentBranch });
-  const filenames = diff.committedFiles;
+const UnknownRevisionError = Error('Unknown revision or path not in the working tree');
 
+const getCommittedFilesnames = (branch) => {
+  const options = { baseBranch: `origin/${branch}` };
+
+  return gitChangedFiles(options)
+    .then(diff => diff.committedFiles)
+    .catch(() => {
+      throw UnknownRevisionError;
+    })
+};
+
+const getPatterns = config => Object.keys(config);
+
+const handleExecutionError = (err) => {
+  process.exit(err === UnknownRevisionError ? 0 : 1);
+};
+
+const executeBeforePush = async (config) => {
   try {
-    Object.keys(config).map((pattern) => {
+    const currentBranch = await getCurrentBranchName();
+    const filenames = await getCommittedFilesnames(currentBranch);
+
+    getPatterns(config).map((pattern) => {
       const match = micromatch(filenames, [pattern]);
 
       if (match.length) {
@@ -17,11 +36,11 @@ const executeBeforePush = async (config) => {
       }
     });
   } catch (err) {
-    process.exit(1);
+    handleExecutionError(err);
   }
 };
 
 // TODO: move the config definition to package.json or prepushrc.js
 executeBeforePush({
-  '*': '$echo "Hello world"',
+  '*.js': 'echo "Hello world"',
 });
